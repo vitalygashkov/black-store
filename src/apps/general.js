e19cc577d1411aa054c2e884f3bda6312c5d08dd
Vitaly Gashkov <vitalygashkov@vk.com>
2025-08-28 20:37:07 +0500

    fix: fetching items for general posts, improve link parsing

--- src/apps/general.js
+++ src/apps/general.js
@@ -6,7 +6,7 @@ const RSS_4PDA_ALL =
   'https://4pda.to/forum/index.php?m=2373711&psb=cEM7PkI-PG9tcGxsQjw-QEA6PTw-Pzo-PUNtP2xBQUE_&act=st_rss&st=1046078';
 
 const fetchLatestPosts = async () => {
-  const items = fetchItems(RSS_4PDA_ALL);
+  const items = await fetchItems(RSS_4PDA_ALL);
   const posts = [];
   for (const item of items) {
     const shouldAddPost = hasAppLink(item.description);
@@ -14,9 +14,12 @@ const fetchLatestPosts = async () => {
       const title = parseAppName(item.description);
       const appStoreLink = item.description.split('href="https://apps.')[1]?.split('"')[0];
       const testflightLink = item.description.split('href="https://testflight.')[1]?.split('"')[0];
-      const link = appStoreLink || testflightLink;
+      const link = appStoreLink ? `https://apps.${appStoreLink}` : `https://testflight.${testflightLink}`;
       const exists = posts.find((p) => p.link === link);
       if (exists) continue;
+      if (!title) {
+        console.log(item.description);
+      }
       posts.push({ author: item.title, title, link });
     }
   }
