4b743d7c6c2ff34baf43e66d17400a0a50a64196
Vitaly Gashkov <vitalygashkov@vk.com>
2025-11-29 18:26:07 +0500

    fix: emit posts right after they were parsed; move refresh & throttling delays to env

--- src/feed.js
+++ src/feed.js
@@ -2,6 +2,12 @@ import { setTimeout as sleep } from 'timers/promises';
 import { EventEmitter } from 'node:events';
 import { dateNow } from './util.js';
 
+// Refresh all feed sources (every 2 hours by default)
+const getRefreshInterval = () => (process.env.REFRESH_INTERVAL_MINUTES ?? 120) * 60 * 1000;
+
+// Delay between requests to each source (10-20 minutes by default)
+const getThrottlingDelay = () => (1 + Math.random()) * (process.env.THROTTLING_MINUTES ?? 10) * 60 * 1000;
+
 export class Feed extends EventEmitter {
   constructor(sources = []) {
     super();
@@ -15,7 +21,6 @@ export class Feed extends EventEmitter {
 
   async refresh({ skipEmit = false } = {}) {
     console.log(`[${dateNow()}] Refreshing feed...`);
-    const updates = [];
 
     if (!this.sources.length) {
       console.log(`[${dateNow()}] No sources added to feed`);
@@ -26,26 +31,31 @@ export class Feed extends EventEmitter {
       console.log(`[${dateNow()}] Fetching posts from ${source.url}`);
       const posts = await source.fetchPosts();
       console.log(`[${dateNow()}] Found ${posts.length} posts`);
-      updates.push(...posts);
-      await sleep((1 + Math.random()) * 10 * 60 * 1000); // Random delay 10-20 minutes between requests
-    }
 
-    for (const update of updates) {
-      const existing = this.feed.find((p) => p.link === update.link);
-      if (existing) continue;
-      this.feed.push(update);
-      if (skipEmit) continue;
-      console.log(`[${dateNow()}] [${update.author}] ${update.title}: ${update.link}`);
-      this.emit('update', update);
-      await this.onUpdate?.(update);
+      for (const post of posts) {
+        const existing = this.feed.find((p) => p.link === post.link);
+        if (existing) {
+          console.log(`[${dateNow()}] Post already exists in feed: ${post.link}`);
+          continue;
+        }
+        console.log(`[${dateNow()}] [${post.author}] ${post.title}: ${post.link}`);
+        this.feed.push(post);
+        if (skipEmit) continue;
+        this.emit('update', post);
+        await this.onUpdate?.(post);
+      }
+
+      const delay = getThrottlingDelay();
+      console.log(`[${dateNow()}] Waiting for ${Math.round(delay / 1000)}s before parsing next source...`);
+      await sleep(delay);
     }
   }
 
-  async watch({ interval = 2 * 60 * 1 * 60 * 1000 /* every 2 hours */ } = {}) {
-    await this.refresh();
+  async watch({ interval = getRefreshInterval() } = {}) {
+    const isInit = this.feed.length === 0;
+    await this.refresh({ skipEmit: isInit }); // Notify about new updates only after initialization
     return setInterval(async () => {
-      const isInit = this.feed.length === 0;
-      await this.refresh({ skipEmit: isInit }); // Notify about new updates only after initialization
+      await this.refresh();
     }, interval);
   }
 }
